2cb53355cefb8682179d828fd6c6d6b8b72bdff7
Dominik Childs <dominik-childs@t-online.de>
2020-07-29 00:28:53 +0200

    Adden von neuen unsern die gespiecht werden sollen und ID fetchen funktioniert

--- databaseConfig.py
+++ databaseConfig.py
@@ -1,17 +1,50 @@
-import _sqlite3
-
-# Eine Tabelle mit nur Primary Key und User ID
-# Das Skript geht dann durch die BasisDB durch und richtet sich nicht nach einer festen liste im code, so dass man das Programm nicht abbrechen muss.
-# Im zweifel soll man einfach den username eintragen können und nicht die ID, die ID sucht dann ggf. das Programm selbst raus
-
-# BasisDB = Primary Key, InstaUserID, Username
-# DatenDB = Primarykey, MediaID bei Download (Doppelte downloads vermeiden), Uhrzeit in Unixtime, MediaType (Video oder Image), Dateinamen nach erfolgtem Download
-
+import sqlite3
+from igramscraper import instagram
+import time
 
 # ToDo:
-# Datenbank nach oberem vorbild erstellen 20 min
-# Prozedur um nur Username einzutragen und dann die ID zu beziehen und nachzutragen in der DB. Ansonsten ID benutzen und nicht username 10 Min
+
 # Download von Datein und speichern im Ordner mit der ID des Users in der DB mit Dateinamen der in der DB steht 10 Min
 # Ggf. anlegen von Verzeichnissen für User  30 Min
 # Loop schreiben für dauerhafte ausführung nach bestimmter Zeitspanne (while True except break) 5 Min
 
+class DatabaseFunctions:
+    def __init__(self):
+        self.dbName = "storydetails.db"
+    def createDatabase(self):
+        dbName = self.dbName
+        conn = sqlite3.connect(dbName)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE baseTable
+                     (dbID INTEGER PRIMARY KEY AUTOINCREMENT, instaID text, username text)''')
+        c.execute('''CREATE TABLE storyInfo
+                     (userID INTEGER, mediaID text, unixtime text, mediaType text, highResImg text, StdResImg text, LowResImg text, LowResVid text, HighResVid text, StdResVid text, FOREIGN KEY(userID) REFERENCES baseTable(dbID))''')
+        conn.commit()
+        conn.close()
+        return dbName
+    def writeInstaIDIntoDB(self,instaID,username):
+        conn = sqlite3.connect(self.dbName)
+        c = conn.cursor()
+        c.execute("""UPDATE baseTable SET instaID = ? WHERE username =? """,(instaID,username,))
+        conn.commit()
+        conn.close()
+    def getAllUserIDs(self):
+        conn = sqlite3.connect(self.dbName)
+        c = conn.cursor()
+        c.execute("""SELECT instaID,dbID FROM baseTable""")
+        ids = c.fetchall()
+        conn.close()
+        return ids
+
+    def withoutID(self):
+        conn = sqlite3.connect(self.dbName)
+        c = conn.cursor()
+        c.execute("""SELECT username,dbID FROM baseTable WHERE instaID IS NULL""")
+        withoutIDs =  c.fetchall()
+        conn.close()
+        return withoutIDs
+
+
+
+#idk = DatabaseFunctions()
+#idk.createDatabase()
--- downloadInstaStory.py
+++ downloadInstaStory.py
@@ -2,12 +2,12 @@ from igramscraper.instagram import Instagram
 from config import USERNAME, PASSWORD, MAINPASSWORD, MAINUSERNAME
 import requests
 import sqlite3
+from databaseConfig import DatabaseFunctions
 
-
-instagram = Instagram()
+'''instagram = Instagram()
 instagram.with_credentials(MAINUSERNAME, MAINPASSWORD)
 instagram.login(force=False,two_step_verificator=True)
-ACCOUNTNAME = "pewdiepie"
+ACCOUNTNAME = "sarah._.wi"
 account = instagram.get_account(ACCOUNTNAME)
 
 
@@ -17,18 +17,65 @@ listOfUsers = [account.identifier]  #In die Liste werden die Leute bzw. die IDs 
 
 
 stories = instagram.get_stories(listOfUsers)
-print(stories)
 for story in stories:
-    print(story.owner)
-    print(story.owner.id)
+    print(story.owner)  #User ID von instagram
+    print(story.owner.identifier)
     for oneStory in story.stories:
-        print(oneStory)
         print("Created time")
         print(oneStory.created_time)        #gives back unixtime
-        print("Video URL")
-        print(oneStory.video_standard_resolution_url)   #soll video url speichern wenn story ein video ist.
-        print(oneStory.video_low_resolution_url)
-        print(oneStory.video_low_bandwith_url)
+        print("Story ID")
+        print(oneStory.identifier)
+        if oneStory.type == "image":
+            print("Image URL")
+            print("image_high_resolution_url")
+            print(oneStory.image_high_resolution_url)
+            print("Image low res URL")
+            print(oneStory.image_low_resolution_url)
+            print("Image Standard Res URL")
+            print(oneStory.image_low_resolution_url)
+
+        elif oneStory.type == "video":
+            print("Video URL")
+            try:
+                print(oneStory.video_standard_resolution_url)   #soll video url speichern wenn story ein video ist.
+            except AttributeError:
+                print("No standard resolution url")
+            try:
+                print(oneStory.video_low_resolution_url)
+            except AttributeError:
+               print("No Low Res URL Video")
+
+            try:
+                print(oneStory.video_low_bandwith_url)
+            except AttributeError:
+                print("No low bandwith url")
+'''
 
 #for story in stories.stories:
 #    print(story)
+
+
+class InstaFunctions(Instagram):
+    def __init__(self):
+        Instagram.__init__(self)
+        self.databaseFunc = DatabaseFunctions()
+    def checkDBifNewUser(self): #Vor jedem neuen durchlauf wird diese funktion aufgerufen um zu schauen ob alles klar geht
+        withoutID = self.databaseFunc.withoutID()
+        if len(withoutID)!= 0:
+            print(withoutID)
+            for pairs in withoutID:
+                username = pairs[0]
+                print(username)
+                user = self.get_account(username=username)
+                identifier = user.identifier
+                self.databaseFunc.writeInstaIDIntoDB(identifier,username)
+
+
+
+
+instagram = InstaFunctions()
+instagram.with_credentials(MAINUSERNAME, MAINPASSWORD)
+instagram.login(force=False, two_step_verificator=True)
+
+
+instagram.checkDBifNewUser()
